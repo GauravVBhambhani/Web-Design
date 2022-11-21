433e0f16fb8e1c6df118d87b1ac835004bec580b
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2022-11-21 17:08:26 -0500

    minor ui updates

--- Assignment 9/assignment_nine/src/pages/About.js
+++ Assignment 9/assignment_nine/src/pages/About.js
@@ -1,11 +1,18 @@
 import React from 'react'
+// import '../style/About.css';
+import Post from '../components/Post'
 
-function About(){
-    return (
-        <div>
-            About page
-        </div>
-    );
+class About extends React.Component {
+    render() {
+        return (
+            <div className='about-cards'>
+                <Post companyName="About Page" 
+                jobPosition="Some information about assignment"/>
+                
+            </div>
+            
+        );
+    }
 }
 
 export default About
